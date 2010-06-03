d48e168a52cd733bf6efbaa6ff6303fb2a225284
Sebastian Billaudelle <stein@cream-project.org>
2010-06-03 21:31:53 +0200

    Using PulseAudio bindings (http://github.com/sbillaudelle/python-pulseaudio) now instead of ALSA.

--- volume.py
+++ volume.py
@@ -1,8 +1,14 @@
-import alsaaudio as alsa
-import gtk
-
 import cream
 import cream.ipc
+
+import pulseaudio
+
+HOTKEYS = {
+    'raise-volume': 'XF86AudioRaiseVolume',
+    'lower-volume': 'XF86AudioLowerVolume',
+    'mute-volume': 'XF86AudioMute',
+    'change-sink': '<Ctrl>XF86AudioMute'
+    }
 
 class VolumeControl(cream.Module):
 
@@ -12,34 +18,34 @@ class VolumeControl(cream.Module):
 
         self.hotkey_manager = cream.ipc.get_object('org.cream.hotkeys', '/org/cream/hotkeys')
         self.hotkey_broker = cream.ipc.get_object('org.cream.hotkeys', self.hotkey_manager.register(), interface='org.cream.hotkeys.broker')
-        self.hotkey_broker.set_hotkey('raise-volume', 'XF86AudioRaiseVolume')
-        self.hotkey_broker.set_hotkey('lower-volume', 'XF86AudioLowerVolume')
-        self.hotkey_broker.set_hotkey('mute-volume', 'XF86AudioMute')
+
+        for action, hotkey in HOTKEYS.iteritems():
+            self.hotkey_broker.set_hotkey(action, hotkey)
+
         self.hotkey_broker.connect_to_signal('hotkey_activated', self.hotkey_activate_cb)
 
-        self.mixer = alsa.Mixer()
+        self.pulse = pulseaudio.PulseAudio('Volume Control Service')
+        self.pulse.connect()
 
 
     def hotkey_activate_cb(self, action):
+
+        volume = self.pulse.get_volume()
+        mute = self.pulse.get_mute()
 		
         if action == 'raise-volume':
-            vol = self.mixer.getvolume()[0]
-            if vol <= 95:
-                self.mixer.setvolume(vol + 5)
-            else:
-                self.mixer.setvolume(100)
+            vol = (min(100, volume[0] + 5), min(100, volume[1] + 5))
+            self.pulse.set_volume(vol)
         elif action == 'lower-volume':
-            vol = self.mixer.getvolume()[0]
-            if vol >= 5:
-                self.mixer.setvolume(vol - 5)
-            else:
-                self.mixer.setvolume(0)
+            vol = (max(0, volume[0] - 5), max(0, volume[1] - 5))
+            self.pulse.set_volume(vol)
         elif action == 'mute-volume':
-            mute = self.mixer.getmute()[0]
-            if mute:
-                self.mixer.setmute(False)
-            else:
-                self.mixer.setmute(True)
+            self.pulse.set_mute(not mute)
+        elif action == 'change-sink':
+            sinks = self.pulse.get_sinks().keys()
+            for i in xrange(0, len(sinks)):
+                if sinks[i] == self.pulse.get_default_sink():
+                    self.pulse.set_default_sink(sinks[(i + 1) % len(sinks)])
 
 
 if __name__ == '__main__':
