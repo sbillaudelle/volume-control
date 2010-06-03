10a12f23ce108986f1aae8c0697dfb7d8d8175b0
Sebastian Billaudelle <stein@cream-project.org>
2010-06-03 21:44:26 +0200

    Removed cream.ipc-import.

--- src/volume.py
+++ src/volume.py
@@ -1,14 +1,6 @@
 import cream
-import cream.ipc
 
 import pulseaudio
-
-HOTKEYS = {
-    'raise-volume': 'XF86AudioRaiseVolume',
-    'lower-volume': 'XF86AudioLowerVolume',
-    'mute-volume': 'XF86AudioMute',
-    'change-sink': '<Ctrl>XF86AudioMute'
-    }
 
 class VolumeControl(cream.Module):
 
@@ -16,19 +8,13 @@ class VolumeControl(cream.Module):
 
         cream.Module.__init__(self)
 
-        self.hotkey_manager = cream.ipc.get_object('org.cream.hotkeys', '/org/cream/hotkeys')
-        self.hotkey_broker = cream.ipc.get_object('org.cream.hotkeys', self.hotkey_manager.register(), interface='org.cream.hotkeys.broker')
-
-        for action, hotkey in HOTKEYS.iteritems():
-            self.hotkey_broker.set_hotkey(action, hotkey)
-
-        self.hotkey_broker.connect_to_signal('hotkey_activated', self.hotkey_activate_cb)
+        self.hotkeys.connect('hotkey-activated', self.hotkey_activate_cb)
 
         self.pulse = pulseaudio.PulseAudio('Volume Control Service')
         self.pulse.connect()
 
 
-    def hotkey_activate_cb(self, action):
+    def hotkey_activate_cb(self, hotkeys, action):
 
         volume = self.pulse.get_volume()
         mute = self.pulse.get_mute()
